a4c339ac94fa4a398e8cdefd1305d56ae4aa1cfa
Christian Stamati <christian.stamati@gmail.com>
2026-03-26 17:03:23 +0100

    added convex

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,19 +1,17 @@
+import { convexQuery } from "@convex-dev/react-query"
+import { useSuspenseQuery } from "@tanstack/react-query"
 import { createFileRoute } from "@tanstack/react-router"
-import { Button } from "@/components/ui/button"
+import { api } from "convex/_generated/api"
 
 export const Route = createFileRoute("/")({ component: App })
 
 function App() {
+  const { data } = useSuspenseQuery(convexQuery(api.tasks.get))
   return (
-    <div className="flex min-h-svh p-6">
-      <div className="flex min-w-0 max-w-md flex-col gap-4 text-sm leading-loose">
-        <div>
-          <h1 className="font-medium">Project ready!</h1>
-          <p>You may now add components and start building.</p>
-          <p>We&apos;ve already added the button component for you.</p>
-          <Button className="mt-2">Button</Button>
-        </div>
-      </div>
+    <div>
+      {data.map(({ _id, text }) => (
+        <div key={_id}>{text}</div>
+      ))}
     </div>
   )
 }
